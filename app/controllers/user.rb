3b25edbe90be3e5a81d5eb43edcfa06e0a7a0ad2
Brian Tong <btong34@gmail.com>
2013-10-17 10:43:15 -0500

    add addUser view and functionality

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -15,3 +15,19 @@ end
 
 
 ####### POST ###############
+
+post '/users'  do
+
+  if request.xhr?
+    content_type :json
+    user_attributes = JSON.parse(request.body.read)
+    user = User.new(user_attributes)
+    if user.save!
+      user_attributes.to_json
+    else
+      "User NOT saved!"
+    end
+  else
+  end
+  
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,36 @@
 $(document).ready(function() {
   
+  // Snippet taken from StackOverflow to serialize form data into JSON objects
+  $.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+  };  
+
+  //******* BACKBONE MODELS ********//
+
+  var User = Backbone.Model.extend({
+    // backbone knows to append to url depending on HTTP request
+    // e.g. PUT will use '/users/id'
+    // but  POST will simply use '/users'
+    urlRoot: '/users'
+  });
+
 
   //******* BACKBONE COLLECTIONS ********//
 
   var Users = Backbone.Collection.extend({
+    // this sets the destination of a fetch call
     url: '/users'
   });
 
@@ -27,10 +54,38 @@ $(document).ready(function() {
           // _.template(string literal representing HTML, data)
           var template = _.template($('#user-list-template').html(), {users: users.models});
 
-          // $el is backbone helper to reference cached element.
+          // $el is backbone helper to reference cached element in this case .page
           that.$el.html(template);
         }
       })
+    }
+  });
+
+  var EditUser = Backbone.View.extend({
+    el: '.page',
+
+    render: function(){
+      var template = _.template( $('#edit-user-template').html(), {} );
+
+      this.$el.html(template);
+    },
+
+    events: {
+      // Backbone lets us define event listeners like 'click button' : 'doAction'
+      'submit .edit-user-form' : 'saveUser'
+    },
+
+    saveUser: function(event){
+      var userDetails = $(event.currentTarget).serializeObject();
+      var user = new User();
+      user.save(userDetails, {
+        success: function(user){
+          //navigate back to home
+          // by default, navigate only updates url, and doesnt actually trigger route
+          router.navigate('', {trigger: true});
+        }
+      });
+      return false;
     }
   });
 
@@ -38,18 +93,24 @@ $(document).ready(function() {
 
   var Router = Backbone.Router.extend({
     routes: {
-      '' : 'home'
+      '' : 'home',
+      'new':'editUser'
     }
   });
 
   //******* INITIALIZE OBJECTS AND START ********//
 
   var userList = new UserList();
+  var userForm = new EditUser();
 
   var router = new Router();
   
   router.on('route:home', function() {
     userList.render();
+  });
+
+  router.on('route:editUser', function() {
+    userForm.render();
   });
 
   Backbone.history.start();
